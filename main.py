72d8a29ae6357931c3101cf0789b617fe7c69304
yuandongx <yuandongx@126.com>
2022-02-11 09:37:02 +0800

    test

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import tornado.ioloop
 import tornado.web
+import hashlib
+
+def check_signature(param, signature):
+    sha1 = hashlib.sha1()
+    param.sort()
+    for p in param:
+        sha1.update(p.encode())
+    hashcode = sha1.hexdigest()
+    return hashcode == signature
+
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        signature = self.get_argument("signature", None)
+        timestamp = self.get_argument("timestamp", None)
+        nonce = self.get_argument("nonce", None)
+        echostr = self.get_argument("echostr", None)
+        token = "weimaomao" #请按照公众平台官网\基本配置中信息填写
+        checked = check_signature([token, timestamp, nonce], signature)
+        if checked:
+            self.write(echostr)
+        else:
+            self.write("")
+
+    def post(self):
+        pass
 
 
 def make_app():
