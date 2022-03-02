8e3761b12977b6b770bfe080902a76cafd99640b
yuandongx <yuandongx@126.com>
2022-03-02 17:44:41 +0800

    1

--- app/application.py
+++ app/application.py
@@ -6,27 +6,28 @@ from tornado.ioloop import IOLoop
 from tornado.web import Application as App
 
 from .settings import (APP_VAR_PREFIX, APPLICATION_SETTINGS,
-                       DB_CONNECTION_INFO,
+                       DB_CONNECTION,
                        DEFAULT_APP_SETTINGS)
 from .urls import urls
 
 
 def db_url() -> str:
-    return f"mongodb://git"
+    # mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]
+    return "mongodb://{username}:{password}@{host}/{db}?retryWrites=true&w=majority".format(**DB_CONNECTION)
 
 class Application:
     """
     Application
     """
 
-    def __init__(self, port=8888, db_url=""):
+    def __init__(self, port=8888):
         """
         __init__
         :param port:
         """
         self.port = port
         self.db_client = None
-        self.db_url = db_url
+        self.db_url = db_url()
 
     def setup(self):
         """
--- app/settings.py
+++ app/settings.py
@@ -10,11 +10,11 @@ APPLICATION_SETTINGS = {
 }
 # 自定义APP变量的前缀值
 APP_VAR_PREFIX = "wx_"
-DB_CONNECTION_INFO = {
-    "host": "106.75.63.248",
-    "user_name": "",
-    "password": "",
-    "port": 27017,
+DB_CONNECTION = {
+    "host": "106.75.63.248:27017",
+    "username": "test",
+    "password": "123456",
+    "db": "test",
 }
 DEFAULT_APP_SETTINGS = {
     "DEFAULT_DBASE_NAME": "mongo_test",
